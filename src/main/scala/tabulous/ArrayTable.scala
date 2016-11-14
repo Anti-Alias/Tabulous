2c674d014c1589ebfb9300dc0b1d105973c0651b
William Andrew Cahill <sonicandrew@aol.com>
2016-11-13 20:10:48 -0500

    Included docs

--- src/main/scala/tabulous/ArrayTable.scala
+++ src/main/scala/tabulous/ArrayTable.scala
@@ -5,7 +5,7 @@ package tabulous
 * Table that stores original data rather than acting as a view
 * of an existing one.
 */
-case class ArrayTable(override val columns:Array[String], data:Array[Any]) extends Table
+private[tabulous] case class ArrayTable(override val columns:Array[String], data:Array[Any]) extends Table
 {
 	// Checks data
 	require(data.length%columns.length == 0, "Number of data elements must be divisible by the number of columns.  Data invalid.")
